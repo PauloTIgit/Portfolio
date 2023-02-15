8ba5757961c3a7d6edaa86a607a1041ec899fccc
Paulo Fernando Ferreira Pires <92067743+PauloTIgit@users.noreply.github.com>
2023-02-15 12:04:54 -0300

    Update email.php
    Signed-off-by: Paulo Fernando Ferreira Pires <92067743+PauloTIgit@users.noreply.github.com>

--- email.php
+++ email.php
@@ -2,9 +2,8 @@
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $msg = $_POST['menssagem'];
-
-//$to = "suporte@paulodevelop.com.br";
-$to = "pauloferreiradevs@gmail.com";
+//Esse será o e-mail que enviara a mensagem para o destinatario.
+$to = "email@email.com";
 $subject = "Portfolio ";
 
 $menssage ="
@@ -23,36 +22,6 @@ $headers .='From: <pauloferreiradevs@gmail.com>' . "\r\n";
 
 mail($to, $subject, $menssage, $headers);
 
+include("index.php");
+
 ?>
-
-
-<!DOCTYPE html>
-<html lang="pt-BR">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
-    <meta name="description" content="Desenvolvedor Web">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./css/email.css">
-</head>
-
-<body>
-        <div class="container-modal">
-            <div class="modal">
-                <h3 class="text">Email enviado com <span>Sucesso</span> !</h3>
-                <br>
-                <p class="text">Informações enviadas</p>
-                <div class="info">
-                <p>Obrigado pela mensagem entraremos em contato.</p>
-                </div>
-                <div class="contaoner-link">
-                    <a href="./index.html" class="neon">Voltar</a>
-                </div>
-            </div>
-        </div>
-
-</body>
-
-</html>
